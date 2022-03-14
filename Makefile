bc1881c0a8b989d0b951e276ba879585cf68efbc
Gaffer Fitch <gaffer.fitch@gmail.com>
2022-03-14 12:49:42 -0400

    Added level to set apart from env and root optional

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 ACCOUNT=gaf3
 IMAGE=python-micro-logger
 INSTALL=python:3.8.5-alpine3.12
-VERSION?=0.1.0
+VERSION?=0.1.1
 DEBUG_PORT=5678
 TTY=$(shell if tty -s; then echo "-it"; fi)
 VOLUMES=-v ${PWD}/lib:/opt/service/lib \
--- test/test_micro_logger.py
+++ test/test_micro_logger.py
@@ -9,7 +9,9 @@ class TestMicroLogger(unittest.TestCase):
     def test_logger(self):
 
         custom = micro_logger.getLogger("unit-test")
+
         self.assertEqual(custom.name, "unit-test")
+        self.assertEqual(custom.level, logging.WARNING)
 
         root = logging.getLogger()
         self.assertEqual(root.name, "root")
@@ -18,4 +20,14 @@ class TestMicroLogger(unittest.TestCase):
         self.assertEqual(root.handlers[0].__class__.__name__, "StreamHandler")
         self.assertEqual(root.handlers[0].formatter.__class__.__name__, "JsonFormatter")
         self.assertEqual(root.handlers[0].formatter._fmt, "%(created)f %(asctime)s %(name)s %(levelname)s %(pathname)s %(funcName)s %(lineno)d %(message)s")
+        self.assertEqual(root.handlers[0].formatter.datefmt, "%Y-%m-%d %H:%M:%S %Z")
+
+        custom = micro_logger.getLogger("test-unit", level="INFO", root_level=False)
+
+        self.assertEqual(custom.name, "test-unit")
+        self.assertEqual(custom.level, logging.INFO)
+
+        root = logging.getLogger()
+        self.assertEqual(root.name, "root")
+        self.assertEqual(root.level, logging.WARNING)
         self.assertEqual(root.handlers[0].formatter.datefmt, "%Y-%m-%d %H:%M:%S %Z")
